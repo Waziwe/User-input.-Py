41327f3854af7a0b02cc9492a0f991b91c1baea3
Waziwe <149684422+Waziwe@users.noreply.github.com>
2024-09-13 01:35:56 +0200

    Add files via upload

---
+++ user_input.py_013120.py
@@ -0,0 +1,10 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name=input('What is your name?\n')# \n --->
+What is your name?
+Andile
+>>> age=input('What is your age?\n')# \n --->
+What is your age?
+27
+>>> location=input('What is your location?\n')# \n --->
+What is your location?
